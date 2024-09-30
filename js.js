4b3bfa9d1606c09f447639ad5d5ed2daeeae1078
kaue-wg <kaue.gutknecht@gmail.com>
2024-09-30 15:46:43 -0300

    Upload dos arquivos

---
+++ js.js
@@ -0,0 +1,23 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+document.getElementById('formularioIMC').addEventListener('submit', function(){
+    event.preventDefault();//impede o envio do formulario
+    
+    const Numero_Conta = document.formularioIMC.Numero_Conta.value;
+    var CPF = document.formularioIMC.CPF.value;
+    const senha = document.formularioIMC.senha.value;
+    const Frase_secreta = document.formularioIMC.Frase_secreta.value;
+    var situacao;
+    
+
+document.getElementById('mensagemSaida').innerHTML =
+        "Obrigado pelo seus dados,em breve enviaremos uma resposta!";
+        
+     
+//mostra a saida
+document.getElementById('mensagem').style.display = 'block';
+
+});
